bdbdeffea420e4f4baf610c64b030b0169f79f08
HarleyMax <wakemanharley@yahoo.com>
2024-03-24 15:33:01 -0400

    Installed spring security

--- backend/src/main/java/harleyw/harchess/service/SignUpService.java
+++ backend/src/main/java/harleyw/harchess/service/SignUpService.java
@@ -2,22 +2,25 @@ package harleyw.harchess.service;
 
 import harleyw.harchess.model.User;
 import harleyw.harchess.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpService {
     UserRepository userRepository;
+    PasswordEncoder userPasswordEncoder;
 
-    public SignUpService(UserRepository userRepository) {
+    public SignUpService(UserRepository userRepository, PasswordEncoder userPasswordEncoder) {
         this.userRepository = userRepository;
+        this.userPasswordEncoder = userPasswordEncoder;
     }
 
     public boolean signUpUser(User user) {
         if(userRepository.findByUsername(user.getUsername()) != null) {
             return false;
         } else {
-
-            userRepository.addUser(user.getUsername(), user.getPassword(), user.getRating());
+            String hashedPassword = userPasswordEncoder.encode(user.getPassword());
+            userRepository.addUser(user.getUsername(), hashedPassword, user.getRating());
             return true;
         }
     }
